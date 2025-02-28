834d58c6a06466916a441a1226e9dc4fd63ced4d
pnarciso1 <80923225+pnarciso1@users.noreply.github.com>
2025-02-28 14:43:53 -0500

    Add Myasthenia Gravis Patient Program landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+      fontFamily: {
+        raleway: ['var(--font-raleway)'],
+      },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
